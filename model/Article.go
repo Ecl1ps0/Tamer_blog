08d6b959936900609bbcf72c1a93c31b64150ffa
Ecl1ps0 <93914852+Ecl1ps0@users.noreply.github.com>
2023-11-19 23:40:37 +0600

    fix: add id to the article model

--- model/Article.go
+++ model/Article.go
@@ -1,9 +1,12 @@
 package model
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Article struct {
-	Title        string `bson:"title"`
-	TextContent  string `bson:"textContent"`
-	ImageContent string `bson:"imageContent"`
-	CreateAt     uint64 `bson:"createAt"`
-	UpdatedAt    uint64 `bson:"updatedAt"`
+	ID           primitive.ObjectID `bson:"_id, omitempty"`
+	Title        string             `bson:"title"`
+	TextContent  string             `bson:"textContent"`
+	ImageContent string             `bson:"imageContent"`
+	CreateAt     uint64             `bson:"createAt"`
+	UpdatedAt    uint64             `bson:"updatedAt"`
 }
